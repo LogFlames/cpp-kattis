57ad0dbcb5d50b279556ca2a2601850afd1ac039
Elias Lundell <elilun03@gmail.com>
2022-05-20 13:28:56 +0200

    sequences

--- sequences.cpp
+++ sequences.cpp
@@ -1,15 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// #define DEBUG
+
 #define rep(i, a, b) for (int i = a; i < (b); ++i)
 #define per(i, a, b) for (int i = a - 1; i >= (b); --i)
 #define trav(a, x) for(auto& a : (x))
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
-#define deb(x) cout << #x << " = " << (x) << endl
-#define deb2(x, y) cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
-#define debv(a) cout << #a << ": [ "; trav(i,(a)) { cout << i << " "; } cout << "]" << endl
+
+#ifdef DEBUG
+#define deb(x) cerr << #x << " = " << (x) << endl
+#define deb2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debv(a) cerr << #a << ": [ "; trav(i,(a)) { cout << i << " "; } cout << "]" << endl
+#else
+#define deb(x)
+#define deb2(x, y)
+#define debv(a)
+#endif
+
 #define prt(a) cout << a << endl
 #define gtz(a, b) ((a) > 0 ? (b) : 0)
 #define amin(a, b) a = min((a), (b))
@@ -25,6 +35,7 @@ using namespace std;
 #define S second
 
 #define PI 3.1415926535897932
+#define MOD 1000000007
 
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 
@@ -44,17 +55,52 @@ typedef vector<vector<string>> vvs;
 typedef priority_queue<int> pqi;
 typedef priority_queue<ll> pql;
 
+ll mod_power(ll a, ll b, ll m) {
+    ll res = 1ll;
+
+    a %= m;
+
+    while (b > 0) {
+        if (b & 1) {
+            res = (res * a) % m;
+        }
+
+        a = (a * a) % m;
+        b = b >> 1;
+    }
+
+    return res;
+}
+
 /*
-2^(antal ? till vänster om) * ((antal hopp till höger - antal fasta ettor) * (antal ? till höger) - antal ettor i alla binära tall med längd av antal ?) - summera för varje 1 och ?
+2^(antal ? till vänster om) * ((antal hopp till höger - antal fasta ettor) * 2^(antal ? till höger) - antal ettor i alla binära tall med längd av antal ? till höger) - summera för varje 1 och ?
 
 l = 1
 n | a
-1   1
-2   4
-3   12
-4   32
+1   1  | 2^0 * 1
+2   4  | 2^1 * 2
+3   12 | 2^2 * 3
+4   32 | 2^3 * 4
+n      | 2^(n - 1) * n
 
-antal ettor_n = antal ettor_(n-1) * 2 + 2 ^ (n - 1)
+
+??10?
+
+00100 | 2
+00101 | 1
+01100 | 4
+01101 | 2
+10100 | 5
+10101 | 3
+11100 | 6
+11101 | 3
+26
+
+2^0 * ((4 - 1) * 2^(2) - 4) | 8
+2^1 * ((3 - 1) * 2^(1) - 1) | 6
+2^2 * ((2)     * 2^(1) - 1) | 12
+
+
  */
 
 void fast() {
@@ -66,6 +112,36 @@ int main() {
 
     string s;
     getline(cin, s);
+
+    vi ones_acc(sz(s), 0), ques_acc(sz(s), 0);
+    rep(i,0,sz(s)) {
+        ones_acc[i] = gtz(i, ones_acc[i - 1]) + (s[i] == '1' ? 1 : 0);
+        ques_acc[i] = gtz(i, ques_acc[i - 1]) + (s[i] == '?' ? 1 : 0);
+    }
+
+    ll sum = 0ll;
+
+    rep(i,0,sz(s)) {
+        if (s[i] == '0') {
+            continue;
+        }
+        ll left = mod_power(2, gtz(i, ques_acc[i - 1]), MOD);
+        ll jump = (sz(s) - i - 1);
+        ll on = (ones_acc[sz(s) - 1] - ones_acc[i]);
+        ll right = (ques_acc[sz(s) - 1] - ques_acc[i]);
+        ll oiq = (mod_power(2, right - 1, MOD) * right) % MOD;
+
+        deb2(left, jump);
+        deb2(on, right);
+        deb(oiq);
+
+        deb(mod_power(2, right, MOD));
+
+        ll to_add = left * (((jump - on) * mod_power(2, right, MOD) - oiq) % MOD) % MOD;
+        sum = (sum + to_add) % MOD;
+    }
+
+    prt(sum);
 
     return 0;
 }
